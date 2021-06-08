6578d2c9773382004dd0ff5cd801f1a6c38105ca
vPartyzan <peregrim.mt@gmail.com>
2021-06-08 14:04:54 +0200

    Add profile endpoints

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const loggedUser = (req, res, next) => {  
+  (req.user == undefined) ? res.redirect('no-permission') : next();
+};
+
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  req.user ? res.render('logged') : res.redirect('no-permission');
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
+});
+
+router.get('/profile', loggedUser, (req, res) => {
+  res.render('profile');
+});
+
+router.get('/profile/settings', loggedUser, (req, res) => {
+  res.render('settings');
 });
 
 module.exports = router;
